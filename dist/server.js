d37a84a0421b95b73d13550a5be12a7437a59a67
Matheus Mazzola <rzmatheus@live.com>
2022-05-10 21:57:38 -0300

    Backend adjustments before deployment

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,16 @@
 "use strict";
-const test = 1;
-async function asadsad() {
-    return 1;
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json({ limit: "50mb" }));
+app.use(express_1.default.urlencoded({ limit: "50mb" }));
+app.use(routes_1.routes);
+app.listen(3333, () => {
+    console.log("HTTP Server running!");
+});
